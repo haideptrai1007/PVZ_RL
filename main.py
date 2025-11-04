b64ab91d91c41cf75216abb1af8b605c7ed2ffce
haideptrai1007 <haingo10.07.2004@gmail.com>
2025-11-04 12:07:02 +0700

    4.1.1
    Fix CKPT

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     parser.add_argument('--sp', type=int, default=20)
     parser.add_argument('--ep', type=int, default=1000)
     parser.add_argument('--fr', type=int, default=10)
-    parser.add_argument('--ckpt', type=int, default=None)
+    parser.add_argument('--ckpt', type=str, default=None)
     parser.add_argument("--save", type=bool, default=True)
     parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--vc", type=float, default=0.8)
